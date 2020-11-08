5ff872f5e9b201f97e29adf73a0c3fe37e034315
Noa Ehrenhalt <noa.ehrenhalt@gmail.com>
2020-11-08 12:42:15 +0200

    Nov 8, 2020 v7 (cp #1)

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -12,13 +12,17 @@ XPATH_ANIMAL_ID = "//html/body/div[1]/div[2]/div/div/div/div/div[2]/div/div/div/
 ANIMAL_DETAIL_CLASS = "animalDetailItem"
 XPATH_NEXT_PAGE = "/html/body/div[1]/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div[3]/div/div/div/div/div[2]/ul/li[12]/span"
 URL_DETAILS = "https://animalcare.lacounty.gov/view-our-animals/?animalCareCenter=ALL&animalType=ALL&sex=ALL&breed=ALL&animalAge=ALL&animalSize=ALL&animalID=&animalDetail="
+CHROME_DRIVER = "C:\web-drivers\chromedriver.exe"
 
 
 def get_animal_details(driver, ID_list):
     """
     get animal details from page by animal ID
     """
+    # Initializing dictionary to store animal details
     animal_dict = {}
+
+    # iterate over animal IDs and gather animal details
     for ID in ID_list:
         driver.get(URL_DETAILS+ID)
         time.sleep(1)
@@ -32,9 +36,12 @@ def get_animal_details(driver, ID_list):
                 if len(temp_list) < 3:
                     details_list.append(temp_list)
 
+        # puts animal details into user-friendly readable format
         clean_list = clean_animal_list(details_list)
         print(clean_list)
         animal_dict[ID] = clean_list
+
+    # print full dictionary with animal IDs as keys and details as values
     print(animal_dict)
 
 
@@ -88,7 +95,7 @@ def move_to_next_page(driver):
 def main():
 
     # Initiates the connection to the host URL
-    driver = webdriver.Chrome(executable_path='C:\web-drivers\chromedriver.exe')
+    driver = webdriver.Chrome(executable_path=CHROME_DRIVER)
     driver.get(URL)
 
     # view options for next page
@@ -101,12 +108,16 @@ def main():
     ID_list = []
     has_next_page = True
 
+    # Gather animal IDs from every webpage
     while has_next_page:
         ID_list = get_animal_id(driver, ID_list)
         has_next_page = move_to_next_page(driver)
-        
+
+    # Gather animal details from individual animal pages
     get_animal_details(driver, ID_list)
+
     driver.quit()
+
 
 if __name__ == '__main__':
     main()
