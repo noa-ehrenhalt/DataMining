46afc39993dd86e6d3ffd91e5270f1f60aa1783b
Ariana Gordon <ariana.gordon@gmail.com>
2020-11-28 08:54:33 -0500

    Updated id retrieval

--- update_animal_db.py
+++ update_animal_db.py
@@ -13,20 +13,27 @@ sql_data = pd.read_sql_table('breeds', engine)
 df = pd.read_csv('animal_data.csv')
 df.dropna(subset=['Animal ID'], inplace=True)
 
+def get_id(col_id, table, col_name, value):
+    result = conn.execute(f"SELECT {col_id} FROM animal_database.{table} WHERE {col_name}=\'{value}\';")
+    type_id = result.fetchone()
+    return type_id
+
+def update_ids(col_id, table, col_name, value):
+    type_id = get_id(col_id, table, col_name, value)
+    if type_id == None:
+        conn.execute(f"INSERT INTO animal_database.{table} ({col_name}) VALUES (\'{value}\');")
+        type_id = get_id(col_id, table, col_name, value)
+    return type_id
+
 
 conn = engine.connect()
 trans = conn.begin()
 
 for index, row in df.iterrows():
-#    try:
-    result = conn.execute(f"SELECT breed_id FROM animal_database.breeds WHERE breed_name=\'{df['Breed'][index]}\';")
-    breed_id = result.fetchone()
-    if breed_id == None:
-        conn.execute(f"INSERT INTO animal_database.breeds (breed_name) VALUES (\'{df['Breed'][index]}\');")
-#    except:
-#        breed_name = (df['Breed'][index], )
-#        mycursor.execute(add_breed, breed_name)
-#        breed_id = mycursor.lastrowid
+
+    breed_id = update_ids('breed_id', 'breeds', 'breed_name', df['Breed'][index])
+    location_id = update_ids('location_id', 'shelters', 'location_name', df['Location'][index])
+    intake_id = update_ids('intake_id', 'intakes', 'intake_status', df['Intake Status'][index])
 
 trans.commit()
 conn.close()
