c76a83526f94a2b97cf8601115413a3dc9108966
Ariana Gordon <ariana.gordon@gmail.com>
2020-11-28 10:04:42 -0500

    Add data to database (CP2 update_animal_db version) - Nov 28 2020

--- update_animal_db.py
+++ update_animal_db.py
@@ -1,39 +1,69 @@
-import pymysql
+"""
+update_animal_db retrieves information stored in the animal_data.csv file and stores that data in the animal_database
+"""
+from datetime import datetime
 import pandas as pd
 import sqlalchemy as alc
 
-host = "localhost:3306"
-user = "root"
-password = "\'"
-
-engine = alc.create_engine('mysql+pymysql://{}:{}@{}/animal_database'.format(user, password, host))
-sql_data = pd.read_sql_table('breeds', engine)
-
-
-df = pd.read_csv('animal_data.csv')
-df.dropna(subset=['Animal ID'], inplace=True)
 
 def get_id(col_id, table, col_name, value):
+    """
+    Retrieves id of given value from table
+    :param col_id: id to be retrieved
+    :param table: database table where id and value are located
+    :param col_name: column of table housing value
+    :param value: value of whose id will be retrieved
+    :return: retrieved id
+    """
     result = conn.execute(f"SELECT {col_id} FROM animal_database.{table} WHERE {col_name}=\'{value}\';")
     type_id = result.fetchone()
     return type_id
 
+
 def update_ids(col_id, table, col_name, value):
+    """
+    Verifies if given id exists in table and adds id and value if not already present.
+    :param col_id: id to be examined
+    :param table: database table where id and value are located
+    :param col_name: column of table housing value
+    :param value: value corresponding to id
+    :return: matched/retrieved id
+    """
     type_id = get_id(col_id, table, col_name, value)
-    if type_id == None:
+    if type_id is None:
         conn.execute(f"INSERT INTO animal_database.{table} ({col_name}) VALUES (\'{value}\');")
         type_id = get_id(col_id, table, col_name, value)
     return type_id
 
 
+host = "localhost:3306"
+user = "root"
+password = "\'"
+
+add_animal = (f"INSERT INTO animal_database.animals "
+              f"(animal_id, intake_id, location_id, breed_id, sex, age, fixed, intake_date, available_date) "
+              f"VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);")
+
+# Create SQL engine and connect to database
+engine = alc.create_engine('mysql+pymysql://{}:{}@{}/animal_database'.format(user, password, host))
 conn = engine.connect()
 trans = conn.begin()
 
-for index, row in df.iterrows():
+# Read csv and drop empty rows
+df = pd.read_csv('animal_data.csv')
+df.dropna(subset=['Animal ID'], inplace=True)
 
+# Iterate through rows of csv content and add new data to database
+for index, row in df.iterrows():
     breed_id = update_ids('breed_id', 'breeds', 'breed_name', df['Breed'][index])
     location_id = update_ids('location_id', 'shelters', 'location_name', df['Location'][index])
     intake_id = update_ids('intake_id', 'intakes', 'intake_status', df['Intake Status'][index])
+    intake = datetime.strptime(df['Intake Date'][index], '%B %d, %Y').date()
+    available = datetime.strptime(df['Available Date'][index], '%B %d, %Y').date()
+    animal_data = (df['Animal ID'][index], intake_id[0], location_id[0], breed_id[0], df['Sex'][index], df['Age'][index],
+                   df['Fixed'][index], intake, available)
+    conn.execute(add_animal, animal_data)
 
+# Commit changes to database and close connection
 trans.commit()
 conn.close()
